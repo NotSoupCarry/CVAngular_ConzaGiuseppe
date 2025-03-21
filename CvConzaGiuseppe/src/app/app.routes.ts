ed2195b93d100a8700618d2239648dc3b316bec7
Giuseppe Conza <giuseppeforce36@gmail.com>
2025-03-21 19:08:30 +0100

    fixa css

--- CvConzaGiuseppe/src/app/app.routes.ts
+++ CvConzaGiuseppe/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: HomeComponent},
+    {path: "about", component: AboutComponent},
+    {path: "experiences", component: ExperiencesComponent}
+];
