c45a2e54afe27ae22dc4f97bdb2ae08995847e58
Giuseppe Conza <giuseppeforce36@gmail.com>
2025-03-24 01:04:49 +0100

    Update home.component.ts

--- CvConzaGiuseppe/src/app/components/home/home.component.ts
+++ CvConzaGiuseppe/src/app/components/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component  } from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
 
-
 @Component({
   selector: 'app-home',
   standalone: true,
